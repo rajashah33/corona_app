45c79baab4f3f4b9fdfccfad609c3fb1e70689b7
shekhardhar <shekhardhar566@gmail.com>
2020-04-03 10:50:29 +0530

    Fund Page added

--- corona_app/lib/locator.dart
+++ corona_app/lib/locator.dart
@@ -1,6 +1,9 @@
 // import 'package:corona_app/Data/AppConfig.dart';
 import 'package:corona_app/config/AppConfig.dart';
+import 'package:corona_app/config/ReliefText.dart';
+import 'package:corona_app/config/ReliefURL.dart';
 import 'package:corona_app/models/app_data.dart';
+import 'package:corona_app/scopped_models/fund_scoped_model.dart';
 import 'package:corona_app/scopped_models/home_scoped_model.dart';
 import 'package:corona_app/scopped_models/reminder_scoped_model.dart';
 import 'package:get_it/get_it.dart';
@@ -9,7 +12,10 @@ GetIt locator = GetIt.instance;
 
 setupLocator() {
   locator.registerLazySingleton(() => AppConfig());
+  locator.registerLazySingleton(() => ReliefText());
+  locator.registerLazySingleton(() => ReliefURL());
   locator.registerLazySingleton(() => AppData());
   locator.registerLazySingleton(() => HomeScopedModel());
   locator.registerLazySingleton(() => ReminderScopedModel());
+  locator.registerLazySingleton(() => FundScopedModel());
 }
