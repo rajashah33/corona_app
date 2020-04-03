f2bf1895af574683dfd50e0860eb1b2a15eacbb0
shekhardhar <shekhardhar566@gmail.com>
2020-04-03 12:24:24 +0530

    Added trackerpage to web urls

--- corona_app/lib/views/newsList.dart
+++ corona_app/lib/views/newsList.dart
@@ -1,35 +1,41 @@
-
 import 'package:corona_app/config/constants.dart';
 import 'package:corona_app/models/newsArticle.dart';
 import 'package:corona_app/services/webservice.dart';
+import 'package:corona_app/views/tracker_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class NewsListState extends State<NewsList> {
-
-  List<NewsArticle> _newsArticles = List<NewsArticle>(); 
+  List<NewsArticle> _newsArticles = List<NewsArticle>();
 
   @override
   void initState() {
     super.initState();
-    _populateNewsArticles(); 
+    _populateNewsArticles();
   }
 
   void _populateNewsArticles() {
-   
     Webservice().load(NewsArticle.all).then((newsArticles) => {
-      setState(() => {
-        _newsArticles = newsArticles
-      })
-    });
-
+          setState(() => {_newsArticles = newsArticles})
+        });
   }
 
   ListTile _buildItemsForListView(BuildContext context, int index) {
-      return ListTile(
-        title: _newsArticles[index].urlToImage == null ? Image.asset(Constants.NEWS_PLACEHOLDER_IMAGE_ASSET_URL) : Image.network(_newsArticles[index].urlToImage), 
-        subtitle: Text(_newsArticles[index].title, style: TextStyle(fontSize: 18)),
-      );
+    return ListTile(
+      title: _newsArticles[index].urlToImage == null
+          ? Image.asset(Constants.NEWS_PLACEHOLDER_IMAGE_ASSET_URL)
+          : Image.network(_newsArticles[index].urlToImage),
+      subtitle:
+          Text(_newsArticles[index].title, style: TextStyle(fontSize: 18)),
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) =>
+                    TrackerPage(url: _newsArticles[index].url)));
+      },
+    );
   }
 
   @override
@@ -41,13 +47,11 @@ class NewsListState extends State<NewsList> {
         body: ListView.builder(
           itemCount: _newsArticles.length,
           itemBuilder: _buildItemsForListView,
-        )
-      );
+        ));
   }
 }
 
 class NewsList extends StatefulWidget {
-
   @override
-  createState() => NewsListState(); 
+  createState() => NewsListState();
 }
