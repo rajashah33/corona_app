1af16203cf523b17d1aac9ef4f65e15bc3cff842
Raja Shah <rajashah33@gmail.com>
2020-04-03 09:55:45 +0530

    firebase implemented and dynamic links added

--- corona_app/lib/views/news_page.dart
+++ corona_app/lib/views/news_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:corona_app/scopped_models/home_scoped_model.dart';
 import 'package:corona_app/views/base_view.dart';
 import 'package:flutter/cupertino.dart';
@@ -19,11 +20,50 @@ class _NewsPageState extends State<NewsPage> {
     return BaseView<HomeScopedModel>(
       builder: (context, child, model) => Scaffold(
         body: SafeArea(
-          child: ListView(
-            children: <Widget>[Text('news Page')],
-          ),
-        ),
+            child: StreamBuilder(
+          stream: Firestore.instance.collection('covid19Info').snapshots(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (!snapshot.hasData) return Text('Loading...');
+            try {
+              return ListView.builder(
+                itemCount: snapshot.data.documents.length,
+                itemBuilder: (context, index) =>
+                    _buildListItem(context, snapshot.data.documents[index]),
+              );
+            } catch (e) {
+              print(e);
+            }
+          },
+        )),
       ),
+    );
+  }
+
+  _buildListItem(BuildContext context, DocumentSnapshot document) {
+    return ListTile(
+      title: Row(
+        children: <Widget>[
+          Expanded(
+            child: Text(document['name']),
+          ),
+          Expanded(
+            child: Text(document['roll_no'].toString()),
+          ),
+          Expanded(
+            child: Text(document['college']),
+          )
+        ],
+      ),
+      onTap: () {
+        Firestore.instance.runTransaction((transaction) async {
+          // get fresh snapshot of the data of the document
+          DocumentSnapshot freshSnap =
+              await transaction.get(document.reference);
+          await transaction.update(freshSnap.reference, {
+            'name': freshSnap['name'] + 'Shah',
+          });
+        });
+      },
     );
   }
 }
